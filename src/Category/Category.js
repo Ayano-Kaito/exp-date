a806a7121c56000c2c43dfe05a31a8698733f394
Ayano KAITO <ayano.kaito@codeberry.co.jp>
2020-10-29 08:14:15 +0900

    カテゴリー画面のデザイン修正：80%

--- src/Category/Category.js
+++ src/Category/Category.js
@@ -1,38 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './Category.scss';
+
 // 各画面のコンポーネント 
 import AddForm from './AddForm';
-import EditForm from './EditForm';
-import DelForm from './DelForm';
-import Item from '../Item/Item';
+// import EditForm from './EditForm';
+// import DelForm from './DelForm';
+// import Item from '../Item/Item';
 
 class Category extends Component {
-    // スタイルの設定
-    th = {
-        fontSize: "14px"
-    }
-    td = {
-        fontSize: "12px"
-    }
 
     render() {
         let data = this.props.value
+        console.log(data)
         return (
-            <div>
-                <table>
-                <title>カテゴリーを選択してください</title>
-                <p onclick={<AddForm />}>追加</p>
-            　      <tr>
-                        {data.map((value) => (
-                        <th style={this.th}>
-                            <td onClick={<Item categoryId={data.categoryId}/>}>{value}</td>
-                            <td onclick={<EditForm />}>編集</td>
-                            <td onclick={<DelForm />}>削除</td>
-                        </th>
-                        ))}
-                    </tr>
-                </table>
-            </div>
+                <div>
+                    <table className="category">
+                        <tr className="category__header">
+                            <th className="category__header--title" colSpan="2">カテゴリーを選択してください</th>
+                            <th className="category__header--add" onClick={<AddForm />}>追加</th>
+                        </tr>
+                        <tr className="category__info">
+                            <td className="category__info--item">カテゴリー</td>
+                            <td className="category__info--edit">編集</td>
+                            <td className="category__info--delete">削除</td>
+                            {/* {data.map((value) => (
+                                <td style={this.td}>{value.category}</td>
+                                <td style={this.td} onclick={<EditForm />}>編集</td>
+                                <td style={this.td} onclick={<DelForm />}>削除</td>
+                            ))} */}
+                        </tr>
+                    </table>
+                </div>
         )
     }
 }
