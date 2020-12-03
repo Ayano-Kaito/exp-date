cadfc936c5a5baa43be48cbe40727a0764366c9e
Ayano KAITO <ayano.kaito@codeberry.co.jp>
2020-12-03 09:11:42 +0900

    Material-uiを使ってテーブルで表示

--- src/Category/Category.js
+++ src/Category/Category.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from "axios";
-import './Category.scss';
+// import './Category.scss';
+import {Table, TableBody, TableHead, TableCell, TableRow} from '@material-ui/core';
 
 // 各画面のコンポーネント 
 import AddForm from './AddForm';
-// import EditForm from './EditForm';
-// import DelForm from './DelForm';
-// import Item from '../Item/Item';
+import EditForm from './EditForm';
+import DelForm from './DelForm';
+import Item from '../Item/Item';
 
 class Category extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            category: [],
             addCategory: false,
-            category: []
+            Component: null
         };
     }
+
+    selectAdd = () => this.setState({Component: AddForm})
+    selectEdit = () => this.setState({Component: EditForm})
+    selectDel = () => this.setState({Component: DelForm})
+    selectItem = () => this.setState({Component: Item})
 
     componentDidMount() {
         axios.get("/api/categories").then((res) => {
@@ -25,30 +32,28 @@ class Category extends Component {
         });
     }
 
-    addForm() {
-        this.setState((state) => ({
-            addCategory: true,
-        }));
-    }
-
     render() {
+        const {Component} = this.state;
+        if(Component) return <Component />;
         return (
-                <div>
-                    <table className="category">
-                        <tr className="category__header">
-                            <th className="category__header--title" colSpan="2">カテゴリーを選択してください</th>
-                            <th className="category__header--add" onClick={this.addForm}>追加</th>
-                            { this.addCategory && <AddForm />}
-                        </tr>
-                        {this.state.category.map((value) => (
-                            <tr className="category__info">
-                                <td className="category__info--item">{value.category}</td>
-                                <td className="category__info--edit">編集</td>
-                                <td className="category__info--delete">削除</td>
-                            </tr>
-                        ))}
-                    </table>
-                </div>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell colSpan="2">カテゴリーを選択してください</TableCell>
+                        <TableCell onClick={this.selectAdd}>追加</TableCell>
+                        { this.addCategory && <AddForm />}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {this.state.category.map((value) => (
+                        <TableRow key={value.categoryId}>
+                            <TableCell onClick={this.selectItem}>{value.category}</TableCell>
+                            <TableCell onClick={this.selectEdit}>編集</TableCell>
+                            <TableCell onClick={this.selectDel}>削除</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
         )
     }
 }
