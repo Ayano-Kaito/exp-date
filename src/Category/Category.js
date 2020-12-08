676d0288376595da37295f772160f2028443ac8b
Ayano KAITO <ayano.kaito@codeberry.co.jp>
2020-12-09 08:55:02 +0900

    追加、編集、削除ボタン押下で各モーダルが表示されるよう修正

--- src/Category/Category.js
+++ src/Category/Category.js
@@ -5,30 +5,39 @@ import axios from "axios";
 import {Table, TableBody, TableHead, TableCell, TableRow} from '@material-ui/core';
 
 // 各画面のコンポーネント 
-import AddForm from './AddForm';
-import EditForm from './EditForm';
-import DelForm from './DelForm';
+import AddModal from './AddModal';
+import EditModal from './EditModal';
+import DelModal from './DelModal';
 import Item from '../Item/Item';
 
 class Category extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            category: [],
+            categories: [],
             addCategory: false,
-            Component: null
+            Component: null,
+            isCategoryAddModal: false,
+            isCategoryEditModal: false,
+            isCategoryDelModal: false
         };
     }
 
-    selectAdd = () => this.setState({Component: AddForm})
-    selectEdit = () => this.setState({Component: EditForm})
-    selectDel = () => this.setState({Component: DelForm})
-    selectItem = () => this.setState({Component: Item})
+    isShowAddModal(boolean) {
+        this.setState({ isCategoryAddModal: boolean })
+    }
+    isShowEditModal(boolean) {
+        this.setState({ isCategoryEditModal: boolean })
+    }
+    isShowDelModal(boolean) {
+        this.setState({ isCategoryDelModal: boolean })
+    }
+    isShowItem = () => this.setState({Component: Item})
 
     componentDidMount() {
         axios.get("/api/categories").then((res) => {
-            this.setState({ category: res.data })
-            console.log(this.state.category)
+            this.setState({ categories: res.data })
+            console.log(this.state.categories)
         });
     }
 
@@ -40,19 +49,27 @@ class Category extends Component {
                 <TableHead>
                     <TableRow>
                         <TableCell colSpan="2">カテゴリーを選択してください</TableCell>
-                        <TableCell onClick={this.selectAdd}>追加</TableCell>
-                        { this.addCategory && <AddForm />}
+                        <TableCell onClick={() => this.isShowAddModal(true)}>追加</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {this.state.category.map((value) => (
-                        <TableRow key={value.categoryId}>
-                            <TableCell onClick={this.selectItem}>{value.category}</TableCell>
-                            <TableCell onClick={this.selectEdit}>編集</TableCell>
-                            <TableCell onClick={this.selectDel}>削除</TableCell>
+                    {this.state.categories.map((category) => (
+                        <TableRow key={category.categoryId}>
+                            <TableCell onClick={this.isShowItem}>{category.categoryName}</TableCell>
+                            <TableCell onClick={() => this.isShowEditModal(true)}>編集</TableCell>
+                            <TableCell onClick={() => this.isShowDelModal(true)}>削除</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
+                { this.state.isCategoryAddModal && (
+                    <AddModal />
+                )}
+                { this.state.isCategoryEditModal && (
+                    <EditModal />
+                )}
+                { this.state.isCategoryDelModal && (
+                    <DelModal />
+                )}
             </Table>
         )
     }
