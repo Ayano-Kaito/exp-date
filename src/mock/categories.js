0d451eb11bd3472a74f62bf9c49bfc2387bb2094
Ayano KAITO <ayano.kaito@codeberry.co.jp>
2020-12-14 09:20:37 +0900

    mockデータ修正

--- src/mock/categories.js
+++ src/mock/categories.js
@@ -3,25 +3,25 @@ import axiosMockAdapter from 'axios-mock-adapter';
 
 const mock = new axiosMockAdapter(axios);
 
-const category = [
+const categories = [
     {
         "categoryId": 1,
-        "category": "食材"
+        "categoryName": "食材"
     },
     {
         "categoryId": 2,
-        "category": "調味料"
+        "categoryName": "調味料"
     },
     {
         "categoryId": 3,
-        "category": "冷凍食品"
+        "categoryName": "冷凍食品"
     }
 ]
 
-mock.onGet("/api/categories").reply(200, category);
+mock.onGet("/api/categories").reply(200, categories);
 
-mock.onPost("/api/categories", { params: {category: "飲み物"} }).reply(201, {categoryId: 4});
+mock.onPost("/api/categories", { params: {categoryName: "飲み物"} }).reply(201, {categoryId: 4});
 
-mock.onPut("/api/categories", { params: {categoryId: 4, category: "飲み物"} }).reply(200, {categoryId: 4});
+mock.onPut("/api/categories", { params: {categoryId: 4, categoryName: "飲み物"} }).reply(200, {categoryId: 4});
 
 mock.onDelete("/api/categories", { params: {categoryId: 1} }).reply(200);
