b988bbce92022fb826403324a7964901bd389c62
Ayano KAITO <ayano.kaito@codeberry.co.jp>
2021-03-02 09:43:14 +0900

    mockが表示されるよう修正

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Category from './Category/Category';
+import mock from './mock/categories';
+
+if (window.location.host === 'localhost:3000') {
+  mock.initMock()
+}
 
 export default class App extends React.Component {
 
