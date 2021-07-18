77b48468b756f2a8521dff219cb0b047f41b2be5
Ayano KAITO <ayano.kaito@codeberry.co.jp>
2021-07-18 19:18:46 +0900

    [item]デザイン修正　アイテム一覧画面　対応中

--- src/Item/Item.tsx
+++ src/Item/Item.tsx
@@ -5,8 +5,14 @@ import dayjs from 'dayjs';
 
 // デザイン
 import './Item.scss';
-import { Table, TableHead, TableBody, TableCell, TableRow, IconButton } from '@material-ui/core';
+import { Table, TableHead, TableBody, TableCell, TableRow, Grid, GridList, GridListTile, GridListTileBar, IconButton } from '@material-ui/core';
 import Pagination from '@material-ui/lab/Pagination';
+// アイコン
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined'; // ホーム
+import AppsOutlinedIcon from '@material-ui/icons/AppsOutlined'; // サムネイル
+import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined'; // リスト
+import AddOutlinedIcon from '@material-ui/icons/AddOutlined'; // 追加
+import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined'; // 削除
 
 // 各画面のコンポーネント 
 import AddModal from './ItemAddModal';
@@ -14,9 +20,9 @@ import EditModal from './ItemEditModal';
 import DeleteModal from './ItemDeleteModal';
 
 enum EventType {
-	Add,
-	Edit,
-	Delete
+  Add,
+  Edit,
+  Delete
 }
 
 interface ItemProps {
@@ -30,7 +36,7 @@ interface ItemProps {
 
 interface ItemState {
   selectedItem: item | null,
-	eventType: EventType | null,
+  eventType: EventType | null,
 }
 
 interface ItemsType {
@@ -58,16 +64,16 @@ export default function Item(props: ItemProps) {
   });
   const [select, setSelect] = React.useState<ItemState["selectedItem"]>(null);
   const [event, setEvent] = React.useState<ItemState["eventType"]>(null);
-  const [isDisplay, setIsDisplay ] = React.useState("list");
+  const [isDisplay, setIsDisplay] = React.useState("list");
   const [page, setPage] = React.useState(1); // ページ番号
   const [totalPage, setTotalPage] = React.useState(1); //総ページ数
   const [displayedItems, setDisplayedItems] = React.useState<ItemsType["items"]>(); //表示データ
-  const displayNum = 1; //1ページあたりの表示数
+  const [displayNum, setDisplayNum] = React.useState(2); //1ページあたりの表示数
 
   React.useEffect(() => {
     axios.get("/api/items").then((res) => {
       setItemInfo(res.data);
-      setTotalPage(res.data.totalResults / displayNum);
+      setTotalPage(Math.ceil(res.data.totalResults / displayNum));
       setDisplayedItems(res.data.items.slice(((page - 1) * displayNum), page * displayNum));
     })
       .catch((e) => {
@@ -98,19 +104,39 @@ export default function Item(props: ItemProps) {
   };
 
   const clearEvent = () => {
-		setEvent(null);
-	};
+    setEvent(null);
+  };
 
   const isExpired = (limit: string) => {
-    const today = dayjs().format().slice(0 , 10)
+    const today = dayjs().format().slice(0, 10)
     const expired = today > limit
     if (expired) {
       return '期限切れ'
     }
   }
 
+  const itemsPage = (displayNum: number) => {
+    // 必ず1ページ目を表示
+    const displayPage = 1;
+    setPage(1);
+    setTotalPage(Math.ceil(itemInfo.totalResults / displayNum));
+    setDisplayedItems(itemInfo?.items.slice(((displayPage - 1) * displayNum), displayPage * displayNum));
+  }
+
   const displayChange = (display: string) => {
     setIsDisplay(display);
+    if (display === "thumbnail") {
+      const number = 50;
+      // 表示するアイテム数を変更し、総ページ数と表示するアイテムを変更
+      setDisplayNum(number);
+      itemsPage(number);
+    }
+    if (display === "list") {
+      const number = 2;
+      // 表示するアイテム数を変更し、総ページ数と表示するアイテムを変更
+      setDisplayNum(number);
+      itemsPage(number);
+    }
   }
 
   const changePage = (event: any, index: number) => {
@@ -119,43 +145,70 @@ export default function Item(props: ItemProps) {
   }
 
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>
-            <Link to={`/`}>戻る</Link>
-          </TableCell>
-          <TableCell><IconButton onClick={() => displayChange("thumbnail")}>サ</IconButton></TableCell>
-          <TableCell><IconButton onClick={() => displayChange("list")}>リ</IconButton></TableCell>
-          <TableCell colSpan={3}>{itemInfo?.categoryName}</TableCell>
-          <TableCell align="right" onClick={addItem}>＋</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody className={`Item__${isDisplay ? "list" : "thumbnail"}}`}>
-        {displayedItems && (
-          displayedItems.map((item) => (
-            <TableRow key={item.itemId}>
-              <TableCell><img src={item.imagePath} alt="画像"></img></TableCell>
-              <TableCell onClick={() => editItem(item)}>{item.itemName}</TableCell>
-              <TableCell onClick={() => deleteItem(item)}><IconButton>削除</IconButton></TableCell>
-              <TableCell>{item.limitDate}</TableCell>
-              <TableCell>{isExpired(item.limitDate)}</TableCell>
-              <TableCell>{item.stock}</TableCell>
-              <TableCell>{item.remark}</TableCell>
-            </TableRow>
-          ))
+    <>
+      <Table>
+        <TableHead>
+          <TableRow className="Item__header">
+            <TableCell>
+              <Link to={`/`}><HomeOutlinedIcon /></Link>
+            </TableCell>
+            <TableCell onClick={() => displayChange("thumbnail")}><AppsOutlinedIcon /></TableCell>
+            <TableCell onClick={() => displayChange("list")}><MenuOutlinedIcon /></TableCell>
+            <TableCell colSpan={3}>{itemInfo?.categoryName}</TableCell>
+            <TableCell align="right" onClick={addItem}><AddOutlinedIcon /></TableCell>
+          </TableRow>
+        </TableHead>
+        {isDisplay === "list" ? (
+          <TableBody className="Item__list">
+            {displayedItems && (
+              displayedItems.map((item) => (
+                <TableRow key={item.itemId}>
+                  <TableRow className="Item">
+                    <TableCell><img src={item.imagePath} alt="画像" /></TableCell>
+                    <TableCell onClick={() => editItem(item)}>{item.itemName}</TableCell>
+                    <TableCell onClick={() => deleteItem(item)}><DeleteOutlineOutlinedIcon /></TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>{item.limitDate}</TableCell>
+                    <TableCell>{isExpired(item.limitDate)}</TableCell>
+                    <TableCell>{item.stock}</TableCell>
+                    <TableCell>{item.remark}</TableCell>
+                  </TableRow>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        ) : (
+          <Grid className="Item__thumbnail">
+            {displayedItems && (
+              displayedItems.map((item) => (
+                <GridList key={item.itemId}>
+                  <GridListTile><img src={item.imagePath} alt="画像" />
+                    <GridListTileBar
+                      title={item.itemName}
+                      actionIcon={
+                        <IconButton onClick={() => deleteItem(item)}>
+                          <DeleteOutlineOutlinedIcon />
+                        </IconButton>
+                      }
+                    />
+                  </GridListTile>
+                </GridList>
+              ))
+            )}
+          </Grid>
         )}
-      </TableBody>
+      </Table>
       {event === EventType.Add && (
-				<AddModal onClose={() => clearEvent()} isOpen={true} categoryName={itemInfo?.categoryName} />
-			)}
+        <AddModal onClose={() => clearEvent()} isOpen={true} categoryName={itemInfo?.categoryName} />
+      )}
       {event === EventType.Edit && (
-				<EditModal onClose={() => clearEvent()} isOpen={true} item={select} categoryName={itemInfo?.categoryName} />
-			)}
+        <EditModal onClose={() => clearEvent()} isOpen={true} item={select} categoryName={itemInfo?.categoryName} />
+      )}
       {event === EventType.Delete && (
-				<DeleteModal onClose={() => clearEvent()} isOpen={true} item={select} />
-			)}
+        <DeleteModal onClose={() => clearEvent()} isOpen={true} item={select} />
+      )}
       <Pagination className="Item__pagination" count={totalPage} variant="outlined" onChange={changePage} page={page} />
-    </Table>
+    </>
   )
 }
